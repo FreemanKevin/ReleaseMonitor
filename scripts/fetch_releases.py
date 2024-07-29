327de21469483018a9f4276374185046630ab5f9
FreemanKevin <freemankevin2017@gmail.com>
2024-07-29 15:22:41 +0800

    update files.

--- scripts/fetch_releases.py
+++ scripts/fetch_releases.py
@@ -28,13 +28,32 @@ def get_releases(owner, repo, access_token):
     
     return releases
 
-def save_releases(releases, filename):
+def get_tags(owner, repo, access_token):
+    url = f"https://api.github.com/repos/{owner}/{repo}/tags"
+    headers = {'Authorization': f'token {access_token}'}
+    print(f"Fetching tags from {url}")  # Debug: 显示请求的 URL
+    response = requests.get(url, headers=headers)
+    
+    if response.ok:
+        tags_data = response.json()
+    else:
+        print(f"Failed to fetch data: {response.status_code} {response.text}")  # Debug: 显示错误信息
+        tags_data = []
+    
+    # 提取每个 tag 的名称
+    tags = []
+    for tag in tags_data:
+        tags.append(tag['name'])
+    
+    return tags
+
+def save_data(data, filename):
     directory = "data"
     if not os.path.exists(directory):
         os.makedirs(directory)
     filepath = os.path.join(directory, filename)
     with open(filepath, 'w') as file:
-        json.dump(releases, file, indent=4)
+        json.dump(data, file, indent=4)
     print(f"Saved data to {filepath}")  # Debug: 显示保存文件的路径
 
 # 配置要获取信息的仓库列表
@@ -44,11 +63,17 @@ repositories = [
     ("alibaba", "nacos"),
     ("redis", "redis"),
     ("rabbitmq", "rabbitmq-server"),
-    ("minio", "minio")
+    ("minio", "minio"),
+    ("nginx", "nginx")  # 添加 nginx
 ]
 
-# 遍历列表，获取每个仓库的发布信息并保存
+# 遍历列表，获取每个仓库的发布信息或标签并保存
 for owner, repo in repositories:
-    releases = get_releases(owner, repo, access_token)
-    filename = f"{repo}_releases.json"
-    save_releases(releases, filename)
+    if repo == "nginx":
+        tags = get_tags(owner, repo, access_token)
+        filename = f"{repo}_tags.json"
+        save_data(tags, filename)
+    else:
+        releases = get_releases(owner, repo, access_token)
+        filename = f"{repo}_releases.json"
+        save_data(releases, filename)
